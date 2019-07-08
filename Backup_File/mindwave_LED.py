495dcaf3f8dc7e22b61af3dbea014e00124573b5
shivampatel623 <shivampatel623@gmail.com>
2019-07-08 22:46:43 +0000

    rewrote mindwave_LED

---
+++ Backup_File/mindwave_LED.py
@@ -0,0 +1,61 @@
+import mindwave, time
+import RPi.GPIO as GPIO
+
+# initialise PWM pin
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+
+# setup headset
+headset = mindwave.Headset('/dev/ttyUSB0', 'DB00')
+time.sleep(2)
+
+headset.connect()
+print "Connecting..."
+
+while headset.status != 'connected':
+    time.sleep(0.5)
+    if headset.status == 'standby':
+        headset.connect()
+        print "Retrying connect..."
+print "Connected."
+
+lightswitch = False
+previous = 0
+text = raw_input("Type Attention or Meditation: ")
+if text == "Attention":
+    while True:
+        time.sleep(1)
+        curr = headset.attention
+        print "Attention: %s" % (curr)    
+        if curr >= 50 and previous < 50 :
+            if lightswitch == False :
+                GPIO.output(17,GPIO.HIGH)
+                time.sleep(1)
+                lightswitch = True
+            else :
+                GPIO.output(17,GPIO.LOW)
+                time.sleep(1)
+                lightswitch = False
+        previous = curr
+else:
+    while True:
+        time.sleep(1)
+        curr = headset.meditation
+        print "Meditation: %s" % (curr)    
+        if curr >= 50 and previous < 50 :
+            if lightswitch == False :
+                GPIO.output(17,GPIO.HIGH)
+                time.sleep(1)
+                lightswitch = True
+            else :
+                GPIO.output(17,GPIO.LOW)
+                time.sleep(1)
+                lightswitch = False
+        previous = curr
+
+
+
+
+
